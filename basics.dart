bb7dbe8846b359570207a3a3551ef13e6942f1b7
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-08-01 23:32:41 +0530

    Operators

--- basics.dart
+++ basics.dart
@@ -1,5 +1,27 @@
 void main() {
-  int? a = 10;
-  a = null;
-  print(a);
+  // int? a = 10;
+  // a = null;
+  // print(a);
+
+  // Operators
+
+  var a = 2;
+  var b = 3;
+
+  var c = a + b;
+  print("Sum = $c");
+
+  var d = a - b;
+  print("Difference = $d");
+
+  var e = a * b;
+  print("Product = $e");
+
+  var f = b ~/ a;
+  print("Quotient = $f");
+  var g = b / a;
+  print("Division = $g");
+
+  var h = b % a;
+  print("Modulus = $h");
 }
